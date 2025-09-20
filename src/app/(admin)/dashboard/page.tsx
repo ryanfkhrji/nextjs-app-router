9231fcc8beb5c124edd545d84ad676b0eb3e2551
ryanfkhrji <ryanfakhroji09@gmail.com>
2025-09-20 18:52:06 +0700

    feat: perbaikan eslint

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+// import { useSession } from "next-auth/react";
+// import { useRouter } from "next/navigation";
+// import { useEffect } from "react";
 
 export default function DashboardPage() {
   return (
